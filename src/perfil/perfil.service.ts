4f8fd75b792902e70639e2b8f6e48036d1bc508a
Gabriel-Baluzzo <gbaluzzo@gmail.com>
2025-05-22 03:21:18 -0300

    pruebo traer perfiles

--- src/perfil/perfil.service.ts
+++ src/perfil/perfil.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { CreatePerfilDto } from './dto/create-perfil.dto';
 import { UpdatePerfilDto } from './dto/update-perfil.dto';
+import { PrismaService } from 'prisma/prisma.service';
 
 @Injectable()
 export class PerfilService {
+  constructor(private readonly prisma: PrismaService) {}
   create(createPerfilDto: CreatePerfilDto) {
     return 'This action adds a new perfil';
   }
 
   findAll() {
-    return `This action returns all perfil`;
+    return this.prisma.perfil.findMany();
   }
 
   findOne(id: number) {
