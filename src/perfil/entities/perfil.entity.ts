4aa279800bfb61c9439ef64c685fda55d4ad12b2
Gabriel-Baluzzo <gbaluzzo@gmail.com>
2025-06-12 19:41:54 -0300

    agrego tipado fuerte

--- src/perfil/entities/perfil.entity.ts
+++ src/perfil/entities/perfil.entity.ts
@@ -1,5 +1,3 @@
-/* eslint-disable prettier/prettier */
-// src/perfil/entities/perfil.entity.ts
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'prisma/prisma.service';
 import { UpdatePerfilDto } from '../dto/update-perfil.dto';
@@ -10,7 +8,7 @@ import { Perfil as PerfilP } from '@prisma/client';
 export class Perfil {
   constructor(
     private readonly prisma: PrismaService,
-    private credencialService: CredencialService, // Asumo que CredencialService es para operaciones CRUD directas sobre Credencial, no para inclusions.
+    private credencialService: CredencialService,
   ) {}
 
   async findMany(): Promise<PerfilP[]> {
@@ -22,7 +20,6 @@ export class Perfil {
   async findOrThrow(id: number): Promise<PerfilP> {
     const perfil = await this.prisma.perfil.findUnique({
       where: { credencialesId: id },
-      // ✅ AÑADIDO: Incluir las relaciones credencial y nivel_actual
       include: { credencial: true, nivel_actual: true },
     });
     if (!perfil) {
@@ -31,19 +28,17 @@ export class Perfil {
     return perfil;
   }
 
-  async update(id: number, data: UpdatePerfilDto) {
+  async update(id: number, data: UpdatePerfilDto): Promise<PerfilP> {
     await this.findOrThrow(id);
     const { credencial, ...perfilData } = data;
 
     if (credencial) {
-      // Asegúrate de que este update sea para la credencial asociada al perfil
-      // El 'id' que se pasa aquí debe ser el 'credencialesId' del perfil.
       await this.credencialService.update(id, credencial);
     }
     return this.prisma.perfil.update({
       where: { credencialesId: id },
       data: perfilData,
-      include: { credencial: true, nivel_actual: true }, // ✅ También asegúrate de incluir nivel_actual aquí si lo necesitas
+      include: { credencial: true, nivel_actual: true },
     });
   }
 
--- src/video/video.controller.ts
+++ src/video/video.controller.ts
@@ -1,4 +1,3 @@
-/* eslint-disable prettier/prettier */
 import { Controller, Get, UseGuards, Query } from '@nestjs/common';
 import { VideoService } from './video.service';
 import { JwtAuthGuard } from 'src/auth/jwt/jwt.guard';
@@ -11,7 +10,7 @@ import { Video } from '@prisma/client';
 @UseGuards(JwtAuthGuard)
 export class VideoController {
   constructor(private readonly videoService: VideoService) {}
-  
+
   @Get()
   async findQuery(
     @CurrentUser() user: JwtPayload,
@@ -19,5 +18,4 @@ export class VideoController {
   ): Promise<Video[]> {
     return this.videoService.findQuery(user.nivel_actual_id, query);
   }
-
 }
